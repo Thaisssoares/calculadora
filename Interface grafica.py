87e8cc624435acd04e54d0db89bcd971d3a52ce9
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-25 16:45:58 -0300

    -Commit Calculadora -Commit

---
+++ Interface grafica.py
@@ -0,0 +1,36 @@
+# Interface gráfica da calculadora
+from Calculadora import *
+import PySimpleGUI as psg
+
+# psg.theme('DarkAmber')
+psg.theme('BluePurple')
+
+frame_layout = [
+    [psg.Radio('Soma', 'rbgOpcoes', default=True, key='soma')],
+    [psg.Radio('Subtração', 'rbgOpcoes', key='sub')]
+]
+layout = [
+    [psg.Text('Num1: '), psg.InputText(key='n1',size=15), psg.Frame('Opções: ', frame_layout)],
+    [psg.Text('Num2: '), psg.InputText(key='n2',size=15)],
+    [psg.Push(),psg.Text('Total'),psg.InputText(key='total', readonly=True, size=10), psg.Push()], # [psg.Text(key='total')],
+    [psg.Push(), psg.Button('Calcular'), psg.Button('Limpar'), psg.Push()],
+]
+
+janela = psg.Window('Calculadora Simples - Soma', layout)
+
+while True:
+    eventos, valores = janela.read()
+
+    if eventos == psg.WIN_CLOSED:
+        break
+    elif eventos == 'Limpar':
+        janela['n1'].update('')
+        janela['n2'].update('')
+        janela['total'].update('')
+        janela['n1'].set_focus(True)
+        janela['soma'].update(True)
+    else:
+        if janela['soma'].get():
+            janela['total'].update(soma(int(valores['n1']), int(valores['n2'])))
+        else:
+            janela['total'].update(sub(int(valores['n1']), int(valores['n2'])))
